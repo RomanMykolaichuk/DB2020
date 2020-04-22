70f6e40378c79cc6cf56dfd5735abb03e073ed13
Roman <Roman@DESKTOP-RB2OHOF>
2020-04-22 13:09:25 +0300

    04 april 2019 properties for Notebook

--- src/main/java/ua/knu/fit/mit31/dbis/NewTableRepository.java
+++ src/main/java/ua/knu/fit/mit31/dbis/NewTableRepository.java
@@ -24,4 +24,26 @@ public interface NewTableRepository extends JpaRepository<NewTable, Integer> {
             value = "SELECT * FROM NEW_TABLE t WHERE t.age > ?1",
             nativeQuery = true)
     Collection<NewTable> findAllOlder(Integer age);
+    
+    @Query(
+            value = "SELECT p.* FROM new_table p\n" +
+"left join child_table c\n" +
+"on p.id = c.parent_id\n" +
+"group by p.id\n" +
+"having count(c.parent_id) <> 0\n" +
+"order by count(c.parent_id) desc;",
+            nativeQuery = true)
+    Collection<NewTable> findParents();
+    
+    @Query(
+            value = "SELECT p.* FROM new_table p\n" +
+"left join child_table c\n" +
+"on p.id = c.parent_id\n" +
+"group by p.id\n" +
+"having count(c.parent_id) = ?1\n" +
+"order by count(c.parent_id) desc;",
+            nativeQuery = true)
+    Collection<NewTable> findByChildrenAmount(Integer parent);
+    
+    
 }
